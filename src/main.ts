60aec1351fbffd6646d68a911488217bdedff847
21120575 <nguyen.thanhtri2112@gmail.com>
2024-10-27 18:08:43 +0700

    setup cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4000', // Địa chỉ frontend của bạn
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Các phương thức HTTP cho phép
+    credentials: true, // Nếu bạn cần gửi cookie
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
