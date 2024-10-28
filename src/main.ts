b4426c931e2780884fd2f9646786988ca2e6e97e
21120575 <nguyen.thanhtri2112@gmail.com>
2024-10-28 13:28:16 +0700

    change origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
+    origin: ["http://localhost:4000","https://frontend-hw-user-registration.vercel.app"],
     methods: 'POST',
   });
   await app.listen(process.env.PORT ?? 3000);
