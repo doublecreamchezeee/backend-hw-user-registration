a0e0c65c24b5a7659bf9f73a90cc3183f9e0781e
21120575 <nguyen.thanhtri2112@gmail.com>
2024-10-28 13:16:04 +0700

    update method

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: "*",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'POST',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
