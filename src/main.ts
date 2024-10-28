d9a040b1e2bc776970d681f5e7c11fe619ba22b7
21120575 <nguyen.thanhtri2112@gmail.com>
2024-10-28 10:59:20 +0700

    fix cors when deploy

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4000', // Địa chỉ frontend của bạn
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Các phương thức HTTP cho phép
-    credentials: true, // Nếu bạn cần gửi cookie
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
